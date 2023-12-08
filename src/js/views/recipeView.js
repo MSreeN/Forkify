17f27a036469c038dd2d0d9bbf480d9053fbfe8d
MSreeN <khyathimnsree369@gmail.com>
2023-12-08 22:12:46 +0530

    imported fractional package for converting decimals to fractions

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,7 @@
 import icons from 'url:../../img/icons.svg';
+import Fraction from 'fractional';
 
+console.log(Fraction);
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;

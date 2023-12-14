4f948c8a3fcf08e722dfe124451d930b34a80d3e
MSreeN <khyathimnsree369@gmail.com>
2023-12-14 22:21:07 +0530

    implemnetd publisher subscriber pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -37,16 +37,8 @@ async function controlRecipes() {
   }
 }
 
-// controlRecipes();
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
 
-//loading the recipe whenever the hash changes
-// window.addEventListener('hashchange', controlRecipes);
-// if you paste the url in other tab it won't load the recipe because there is no hash change because hash was created when you pasted the url so website won't show the recipe
-// We should make use of load event that fires when page was loaded for the first time
-// we should use load event when url is pasted to other tab because hashchange event won't get fired as said above hash was created for the first time there and wasn't changed and we should use hashchange event when hash was changed in same tab, we can't use load event here because load event gets fired when page is loaded for first time and page was already loaded here
-
-// flow => user clicks on anchor tag => anchor tag changes has as set to href => when hash changes window fires hashchange event and executes code
-//looping through these 2 events right after code executes
-['hashchange', 'load'].forEach(event =>
-  window.addEventListener(event, controlRecipes)
-);
+init();

b6635866ecc9e728a7a352345c2235548f09d70d
MSreeN <khyathimnsree369@gmail.com>
2024-01-06 20:48:34 +0530

    error handling in results view

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -3,7 +3,8 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
   render(data) {
-    if (!data) return this.renderError();
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
     this._data = data;
     const markup = this._generateMarkup();
     this._clear();

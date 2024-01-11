65be99dd6608bd709f98b8028ff12cca3fa3aa89
MSreeN <khyathimnsree369@gmail.com>
2024-01-11 21:26:16 +0530

    trying to debug the parcel problem

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -5,7 +5,7 @@ class ResultsView extends View {
   _errorMessage = 'No results found for your query! Please try again';
 
   _generateMarkup() {
-    console.log(this._data);
+    console.log('hello world');
     return this._data.map(this._generateMarkupPreview).join('');
   }
 

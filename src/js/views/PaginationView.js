148d737f676ae50b4a6b6f787f97d1342099e73f
MSreeN <khyathimnsree369@gmail.com>
2024-01-19 21:29:52 +0530

    pagination view

--- src/js/views/PaginationView.js
+++ src/js/views/PaginationView.js
@@ -4,16 +4,27 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkup() {
-    const numPages = this._data.results.length / this._data.resultsPerPage;
-    console.log(this._data.results);
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
     console.log(numPages);
     //page 1 and there are other pages
-
-    //page 1 and there are no other pages
+    if (this._data.page === 1 && numPages > 1) {
+      return 'page 1, others';
+    }
 
     //last page
-
+    if (this._data.page === numPages && numPages > 1) {
+      return 'last page';
+    }
     //other page
+
+    if (this._data.page < numPages) {
+      return 'other pages';
+    }
+
+    //page 1 and there are no other pages
+    return 'there is only page 1';
   }
 }
 

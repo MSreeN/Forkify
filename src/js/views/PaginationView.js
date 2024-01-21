ba7a1eefd3fa2d987e555c39af1b2f525c846f7f
MSreeN <khyathimnsree369@gmail.com>
2024-01-21 21:29:04 +0530

    setting up paginatin buttons

--- src/js/views/PaginationView.js
+++ src/js/views/PaginationView.js
@@ -2,29 +2,50 @@ import View from './view.js';
 
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
-
   _generateMarkup() {
+    const currPage = this._data.page;
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
     console.log(numPages);
     //page 1 and there are other pages
-    if (this._data.page === 1 && numPages > 1) {
-      return 'page 1, others';
+    if (currPage === 1 && numPages > 1) {
+      return `<button class="btn--inline pagination__btn--next">
+      <span>${currPage + 1}</span>
+      <svg class="search__icon">
+        <use href="src/img/icons.svg#icon-arrow-right"></use>
+      </svg>
+    </button>`;
     }
 
     //last page
-    if (this._data.page === numPages && numPages > 1) {
-      return 'last page';
+    if (currPage === numPages && numPages > 1) {
+      return `<!-- <button class="btn--inline pagination__btn--prev">
+      <svg class="search__icon">
+        <use href="src/img/icons.svg#icon-arrow-left"></use>
+      </svg>
+      <span>${currPage - 1}</span>
+    </button>`;
     }
     //other page
 
-    if (this._data.page < numPages) {
-      return 'other pages';
+    if (currPage < numPages) {
+      return `<!-- <button class="btn--inline pagination__btn--prev">
+      <svg class="search__icon">
+        <use href="src/img/icons.svg#icon-arrow-left"></use>
+      </svg>
+      <span>${currPage - 1}</span>
+    </button>
+    <button class="btn--inline pagination__btn--next">
+      <span>${currPage + 1}</span>
+      <svg class="search__icon">
+        <use href="src/img/icons.svg#icon-arrow-right"></use>
+      </svg>
+    </button>`;
     }
 
     //page 1 and there are no other pages
-    return 'there is only page 1';
+    return '';
   }
 }
 

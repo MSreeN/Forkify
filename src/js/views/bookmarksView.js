dcc496f58f221884fb74d955ee8e349b8ccd6ea6
MSreeN <khyathimnsree369@gmail.com>
2024-02-29 21:41:43 +0530

    Eliminated code duplication

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,3 +1,4 @@
+import PreviewView from './previewView.js';
 import View from './view.js';
 
 class BookmarksView extends View {
@@ -6,31 +7,7 @@ class BookmarksView extends View {
 
   _generateMarkup() {
     console.log(this._data);
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-
-  _generateMarkupPreview(results) {
-    const id = window.location.hash.slice(1);
-    console.log(id);
-    return ` 
-        <li class="preview">
-          <a class="preview__link ${
-            results.id === id ? 'preview__link--active' : ''
-          }" href="#${results.id}">
-            <figure class="preview__fig">
-              <img src="${results.image}" alt=${results.title} />
-            </figure>
-            <div class="preview__data">
-              <h4 class="preview__title">${results.title}</h4>
-              <p class="preview__publisher">${results.publisher}</p>
-              <div class="preview__user-generated">
-              <svg>
-                    <use href="src/img/icons.svg#icon-user"></use>
-                  </svg>
-              </div>
-            </div>
-          </a>
-        </li>`;
+    return this._data.map(PreviewView._generateMarkup).join('');
   }
 }
 
